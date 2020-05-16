8e331741cdff34c64fa9c7092f8785ee433f66aa
Duy Ha <duy.ha@dounets.com>
2020-05-16 09:44:57 +0700

    Initial Commit

---
+++ ble-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ble-ios
+//
+//  Created by duyha on 5/16/20.
+//  Copyright © 2020 Duy Ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
